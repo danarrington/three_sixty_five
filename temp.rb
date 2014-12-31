79824dbc5270d4873301e2428be63a7f178c59e1
Dan Arrington <dan.arrington@gmail.com>
2014-12-31 15:08:36 -0800

    Updating temp

--- temp.rb
+++ temp.rb
@@ -1,5 +1,12 @@
-(8..28).each do |i|
+(5..25).each do |i|
   u = User.find(i)
-  u.runs.create(runtype: :walk, distance: i + 0.5)
+  #u.runs.create(runtype: :walk, distance: i + 0.5)
+  u.total_distance = i + 0.5
+  u.save
 end
+#20.times do |i|
+#  name =  Faker::Name.first_name
+#  User.create!(first_name: name, last_name: Faker::Name.last_name, password:'dandandan', email: "dan.arrington+#{name}@gmail.com")
+#end
+
 

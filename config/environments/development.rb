f2b610ab3c4b71191c7f9056dff1b59782696645
Dan Arrington <dan.arrington@gmail.com>
2015-01-04 08:49:08 -0800

    Start setting up email in dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,20 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_options = {:from => 'noreply@365in365.org'}
+
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => "dan.arrington@gmail.com",
+    :password  => "", # SMTP password is any valid API key
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    :domain => 'localhost', # your domain to identify your server when connecting
+}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log

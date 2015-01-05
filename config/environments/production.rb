e632c194256b7cfc94b00b4a43a2768019053391
Dan Arrington <dan.arrington@gmail.com>
2015-01-05 07:32:23 -0800

    fix production domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { :host => '365in365.org' }
+  config.action_mailer.default_url_options = { :host => 'www.365in365.org' }
   config.action_mailer.default_options = {:from => 'noreply@365in365.org'}
 
   config.action_mailer.smtp_settings = {

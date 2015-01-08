d2e220d0b19a2ed5a591d2a430eff4699d5de197
Dan Arrington <dan.arrington@gmail.com>
2015-01-07 18:59:27 -0800

    Pace needed now rounds to two decimal places

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     @runs = Run.where(user: @user).count
     @day = Date.today.yday
     #TODO account for leap years, write a test for this math
-    @needed_pace = ((365 - @user.total_distance) / (365-@day)).round(1)
+    @needed_pace = ((365 - @user.total_distance) / (365-@day)).round(2)
     @run = Run.new
 
     @user_recent_activity = RecentUserActivity.new(@user)
